93cc4eb5bf4975fd3aac1286865ddb4278cfddf2
RamBalakumaran <rambalakumaran5@gmail.com>
2025-10-23 22:47:27 +0530

    build.sh file added

--- build.sh
+++ build.sh
@@ -5,9 +5,10 @@ set -o errexit
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
-echo "Creating and activating a 1GB swap file..."
-fallocate -l 1G /swapfile
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
+echo "Creating and activating a 1GB swap file in /tmp..."
+# MODIFIED PATH: Using /tmp which is a writable directory in Render's build environment
+fallocate -l 1G /tmp/swapfile
+chmod 600 /tmp/swapfile
+mkswap /tmp/swapfile
+swapon /tmp/swapfile
 echo "Swap file is active."
